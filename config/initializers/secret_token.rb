187454ca6a1be2574e71684713be4b7dc52bc7d5
mack-G <hggamahewage@gmail.com>
2013-08-20 22:31:11 -0700

    mackG and Danny's venture start

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+FlavorFinder::Application.config.secret_key_base = 'e1ef6c7238d6a02b50865007de657602e554c4811c0fa63c800b3d5ea04bc7aad64853fe4ee7867f14a40fd7e83bc80c491dafeebfd1ee7403f424a9ab442eb2'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+FlavorFinder::Application.config.session_store :cookie_store, key: '_FlavorFinder_session'
